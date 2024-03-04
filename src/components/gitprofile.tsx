d31f2b76c7f7141dd3b8e593fed72ac6db855ded
Xavier1999-Chen <60979948+Xavier1999-Chen@users.noreply.github.com>
2024-03-04 19:56:11 +0800

    Update gitprofile.tsx

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -73,9 +73,9 @@ const GitProfile = ({ config }: { config: Config }) => {
         }
         const repos = sanitizedConfig.projects.github.manual.projects
           .map((project) => `+repo:${project}`)
-          .join('')+fork:true;
+          .join('');
 
-        const url = `https://api.github.com/search/repositories?q=${repos}&sort=${sanitizedConfig.projects.github.manual.sortBy}&type=Repositories`;
+        const url = `https://api.github.com/search/repositories?q=${${repos}+fork:true}&sort=${sanitizedConfig.projects.github.manual.sortBy}&type=Repositories`;
 
         const repoResponse = await axios.get(url, {
           headers: { 'Content-Type': 'application/vnd.github.v3+json' },
