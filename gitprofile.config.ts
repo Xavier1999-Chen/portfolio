4657de76d4e433ded5400470a4e792bfa8e9aa3a
Xavier1999-Chen <60979948+Xavier1999-Chen@users.noreply.github.com>
2025-06-23 14:04:16 +0800

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -104,8 +104,7 @@ const CONFIG = {
   resume: {
     fileUrl:
       // 'https://1drv.ms/b/c/ed1c9dc9347a2cae/EXc_zJZKN19FurePQLPztEoBBM7cwFyAu39pJMagscn9lQ?e=d1fByv'
-      // 'https://1drv.ms/b/c/ed1c9dc9347a2cae/EYf4kKSi5elDmtDDbCroQKQBkPPmWXMMa9w-y6LgaKwelg?e=H0NK2X'
-      ''
+      'https://1drv.ms/b/c/ed1c9dc9347a2cae/EYf4kKSi5elDmtDDbCroQKQBkPPmWXMMa9w-y6LgaKwelg?e=H0NK2X'
       // 'https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/Xavier1999-Chen/portfolio/blob/main/CV-ChenXingyi.pdf', // Empty fileUrl will hide the `Download Resume` button.
     
   },
